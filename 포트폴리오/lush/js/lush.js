81917eee93896c76fe8b8f3e41b6b432065b79a1
sho7766 <sho7766@naver.com>
2024-02-10 19:14:26 +0900

    0210 commit

--- 포트폴리오/lush/js/lush.js
+++ 포트폴리오/lush/js/lush.js
@@ -1,9 +1,15 @@
+document.querySelector(".lush_value ul li").classList.add("on");
+
 let bg = document.querySelector(".lush_value");
 let circle = document.querySelectorAll(".lush_value ul li");
 
 circle.forEach(function (value, index) {
   value.onmouseenter = function () {
     bg.style.backgroundImage = "url(images/main_value" + (index + 1) + ".jpg)";
+    circle[index].classList.add("on");
+  };
+  value.onmouseleave = function () {
+    circle[index].classList.remove("on");
   };
 });
 
