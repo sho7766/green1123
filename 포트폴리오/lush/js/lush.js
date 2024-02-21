3d8476e73d4ddce7f4b956e3996cdb2791ebec42
sho <sho7766@naver.com>
2024-02-21 17:59:29 +0900

    first commit

--- 포트폴리오/lush/js/lush.js
+++ 포트폴리오/lush/js/lush.js
@@ -1,7 +1,6 @@
-document.querySelector(".lush_value ul li").classList.add("on");
-
 let bg = document.querySelector(".lush_value");
 let circle = document.querySelectorAll(".lush_value ul li");
+let gnb = document.querySelectorAll(".topmain nav ul li a");
 
 circle.forEach(function (value, index) {
   value.onmouseenter = function () {
@@ -10,9 +9,31 @@ circle.forEach(function (value, index) {
   };
   value.onmouseleave = function () {
     circle[index].classList.remove("on");
+    circle.classList.add("on");
   };
 });
 
 bg.onmouseleave = function () {
   bg.style.backgroundImage = "none";
+};
+window.onscroll = function () {
+  let ht = document.documentElement.scrollTop;
+  console.log(ht);
+  if (ht >= 0 && ht < 440) {
+    document.querySelector("header").style.background = "none";
+    document.querySelector("header").style.borderBottom = "none";
+    document.querySelector(".logo").style.backgroundImage =
+      "url(images/logo.svg)";
+    gnb.forEach(function (value, index) {
+      value.style.color = "#fff";
+    });
+  } else {
+    document.querySelector("header").style.background = "white";
+    document.querySelector("header").style.borderBottom = "1px solid #ccc";
+    document.querySelector(".logo").style.backgroundImage =
+      "url(images/logo_black.svg)";
+    gnb.forEach(function (value, index) {
+      value.style.color = "#333";
+    });
+  }
 };
