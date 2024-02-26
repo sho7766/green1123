7658bcf10f55348537c238e3079c59d9d36a9ed2
sho <sho7766@naver.com>
2024-02-26 17:58:33 +0900

    0226 commit

--- 포트폴리오/ottogi/js/script.js
+++ 포트폴리오/ottogi/js/script.js
@@ -12,9 +12,10 @@ function myfnc() {
 
 gnb.forEach(function (a, b) {
   a.onmouseenter = function () {
+    a.classList.add("on");
     bg.style.background = "#fff";
     sub.forEach(function (a, b) {
-      a.style.height = "375px";
+      a.style.height = "358px";
       a.style.background = "#fff";
     });
   };
@@ -24,8 +25,12 @@ inner.onmouseleave = function () {
 };
 
 $(bg).mouseenter(function () {
-  $(".gnb>li>a").css("color", "#333");
-  $(".sub li a").css("color", "#333");
+  $("header.inner").addClass("on");
+  $(".gnb>li>a").addClass("on");
+  $(".sub li a").css("color", "#555");
   $(".lang a").css("color", "#333");
-  $(".sub").css("width", "100%");
+});
+$(bg).mouseleave(function () {
+  $(".gnb>li>a").removeClass("on");
+  $(".lang a").css("color", "#fff");
 });
