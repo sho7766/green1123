9bf636acbeae5f868358b0fc36764d97668c2d97
sho7766 <sho7766@naver.com>
2024-03-05 23:15:53 +0900

    240305 commit

--- 포트폴리오/153logistics/js/script.js
+++ 포트폴리오/153logistics/js/script.js
@@ -2,39 +2,48 @@ let menu = document.querySelectorAll(".deps1");
 let menuA = document.querySelectorAll(".deps1 > li >a");
 let sub = document.querySelectorAll(".deps2");
 
-$(menu).mouseenter(function () {
-  $("header").css("background", "#fff");
+// menu시작
+function menuIn() {
+  $("header").addClass("on");
   $(".deps1_bg").addClass("on");
-  $(".deps1 a").css("color", "#000");
+  $(menuA).addClass("on");
   $(sub).addClass("on");
   $(".deps2  a").css("color", "#000");
-  $(".logo a img").attr("src", "images/logo_b.svg");
-  $(".share_btn").css();
+  $(".logo a").addClass("on");
+}
+function menuOut() {
+  $("header").removeClass("on");
+  $(".deps1_bg").removeClass("on");
+  $(menuA).removeClass("on");
+  $(sub).removeClass("on");
+  $(".logo a").removeClass("on");
+}
+$(menu).mouseenter(function () {
+  menuIn();
 });
 $(menu).mouseleave(function () {
-  $("header").css("background", "none");
-  $(".deps1_bg").removeClass("on");
-  $(".deps1  a").css("color", "#fff");
-  $(sub).removeClass("on");
-  $(".logo a img").attr("src", "images/logo_w.svg");
+  menuOut();
 });
 window.onscroll = function () {
   let ht = document.documentElement.scrollTop;
   if (ht >= 0 && ht < 90) {
-    document.querySelector("header").style.background = "none";
-    document.querySelector(".logo a img").src = "images/logo_w.svg";
-    menuA.forEach(function (value, index) {
-      value.style.color = "#fff";
+    menuOut();
+    $(menu).mouseleave(function () {
+      menuOut();
     });
-  } else {
-    document.querySelector("header").style.background = "#fff";
-    menuA.forEach(function (value, index) {
-      value.style.color = "#000";
-      document.querySelector(".logo a img").src = "images/logo_b.svg";
+  } else if (ht > 90) {
+    menuIn();
+    $(sub).removeClass("on");
+    $(".deps1_bg").removeClass("on");
+    $(menu).mouseleave(function () {
+      menuOut();
+      $("header").addClass("on");
+      $(menuA).addClass("on");
+      $(".logo a").addClass("on");
     });
   }
 };
-
+// menu끝
 $(".btn").on("click", function () {
   $(this).toggleClass("on");
   // $(".autoplay-progress svg").toggleClass("on");
