92e9e8d4ffca47c6fef7a30bbeab4db69c9d5da0
sho7766 <154848321+sho7766@users.noreply.github.com>
2024-03-10 16:01:45 +0900

    240310 COMMIT

--- 포트폴리오/153logistics/js/script.js
+++ 포트폴리오/153logistics/js/script.js
@@ -48,9 +48,12 @@ window.onscroll = function () {
 
 // menu끝;
 
+// main .slide_move play & pause button toggle
 $(".btn").on("click", function () {
   $(this).toggleClass("on");
 });
+
+// service .slide_box 스크롤 효과
 
 // .special section 마우스이벤트
 const sec = document.querySelector(".special");
@@ -71,4 +74,99 @@ window.addEventListener("mousemove", (e) => {
   ySetter(e.y);
 });
 
-// partner view more 버튼
+// goTop 클릭시 상단으로 이동
+$(".goTop_btn").click(function () {
+  $("html,body").animate({ scrollTop: 0 }, 300);
+});
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 500) {
+    $(".goTop_btn").addClass("on");
+  } else {
+    $(".goTop_btn").removeClass("on");
+  }
+});
+
+let ani1 = gsap.timeline();
+ani1.from(".service .tit_box", { autoAlpha: 0, x: -100 });
+ScrollTrigger.create({
+  animation: ani1,
+  trigger: ".service",
+  start: "top",
+});
+
+// let ani1 = gsap.timeline();
+// ani1
+//   .to(".scroll_img ", {
+//     scale: 0,
+//     rotation: 720,
+//     borderRadius: 200,
+//   })
+//   .to("#section1 .item_img", {
+//     scale: 1,
+//     rotation: 720,
+//     borderRadius: 20,
+//   });
+// ScrollTrigger.create({
+//   animation: ani1,
+//   trigger: "#section1",
+//   start: "top top ",
+//   end: "+=2000",
+//   //#section1 시작위치에서 2000픽셀 떨어진 곳에서 끝나도록 설정
+//   // markers: true,
+//   scrub: true,
+//   pin: true,
+// });
+// let ani2 = gsap.timeline({
+//   defaults: { autoAlpha: 0, borderRadius: 200 },
+// });
+// ani2
+//   .from("#section2 .i1", { y: -100 })
+//   .from("#section2 .i2", { y: 100 })
+//   .from("#section2 .i3", { y: -100 });
+// ScrollTrigger.create({
+//   animation: ani2,
+//   trigger: "#section2",
+//   start: "top top ",
+//   end: "+=2000",
+//   //#section1 시작위치에서 2000픽셀 떨어진 곳에서 끝나도록 설정
+//   // markers: true,
+//   scrub: true,
+//   pin: true,
+// });
+
+// let ani3 = gsap.timeline();
+// ani3.from("#section3 .item_img", {
+//   autoAlpha: 0,
+//   y: -100,
+//   ease: "back.out(4)",
+//   stagger: { amount: 3, from: "random" },
+// });
+// ScrollTrigger.create({
+//   animation: ani3,
+//   trigger: "#section3",
+//   start: "top top ",
+//   end: "+=3000",
+//   // markers: true,
+//   scrub: true,
+//   pin: true,
+//   anticipatePin: 1,
+// });
+
+// let ani4 = gsap.timeline();
+// ani4.to("#section4 .item_img", {
+//   autoAlpha: 0,
+//   scale: 3,
+//   width: "100vw",
+//   height: "100vh",
+// });
+// ScrollTrigger.create({
+//   animation: ani4,
+//   trigger: "#section4",
+//   start: "top top ",
+//   end: "+=3000",
+//   // markers: true,
+//   scrub: true,
+//   pin: true,
+//   anticipatePin: 1,
+// });
