9f31a1eecedafd95d0a92201d2f16700df674f22
sho <sho7766@naver.com>
2024-03-11 17:57:39 +0900

    240311 commit

--- 포트폴리오/153logistics/js/script.js
+++ 포트폴리오/153logistics/js/script.js
@@ -28,7 +28,7 @@ $(menu).mouseleave(function () {
 window.onscroll = function () {
   let ht = document.documentElement.scrollTop;
   if (ht >= 0 && ht < 90) {
-    $(".share_btn button").removeClass("on");
+    $(".share_btn").removeClass("on");
   } else {
     $(menu).mouseenter(function () {
       menuIn();
@@ -42,18 +42,69 @@ window.onscroll = function () {
     $("header").addClass("on");
     $(menuA).addClass("on");
     $(".logo a").addClass("on");
-    $(".share_btn button").addClass("on");
+    $(".share_btn").addClass("on");
   }
 };
 
 // menu끝;
 
 // main .slide_move play & pause button toggle
-$(".btn").on("click", function () {
+$(".autoplay-progress .btn").on("click", function () {
   $(this).toggleClass("on");
 });
 
-// service .slide_box 스크롤 효과
+// section .tit 스크롤 효과
+let ani1 = gsap.timeline();
+ani1.from(".service .tit", { x: -100, autoAlpha: 0, duration: 1 });
+ScrollTrigger.create({
+  animation: ani1,
+  trigger: ".service",
+  start: "top 70%",
+});
+let ani2 = gsap.timeline();
+ani2
+  .from(".data .tit", { x: 100, autoAlpha: 0, duration: 1 })
+  .from(".data .txt", { x: 100, autoAlpha: 0, duration: 1 })
+  .from(".data .txt2", { x: 100, autoAlpha: 0, duration: 1 });
+ScrollTrigger.create({
+  animation: ani2,
+  trigger: ".data",
+  start: "top 70%",
+});
+let ani3 = gsap.timeline();
+ani3.from(".special .tit_box .tit", { x: -100, autoAlpha: 0, duration: 1 });
+ScrollTrigger.create({
+  animation: ani3,
+  trigger: ".special",
+  start: "top 90%",
+});
+let ani4 = gsap.timeline();
+ani4
+  .from(".onestop .tit1", { x: -100, autoAlpha: 0, duration: 1 })
+  .from(".onestop .tit2", {
+    x: -100,
+    autoAlpha: 0,
+    duration: 1,
+  });
+ScrollTrigger.create({
+  animation: ani4,
+  trigger: ".onestop",
+  start: "top 90%",
+});
+let ani5 = gsap.timeline();
+ani5.from(".capa .tit_box .tit", { x: -100, autoAlpha: 0, duration: 1 });
+ScrollTrigger.create({
+  animation: ani5,
+  trigger: ".capa",
+  start: "top 70%",
+});
+let ani6 = gsap.timeline();
+ani6.from(".partner .tit", { x: -100, autoAlpha: 0, duration: 1 });
+ScrollTrigger.create({
+  animation: ani6,
+  trigger: ".partner",
+  start: "top 70%",
+});
 
 // .special section 마우스이벤트
 const sec = document.querySelector(".special");
@@ -86,87 +137,3 @@ $(window).scroll(function () {
     $(".goTop_btn").removeClass("on");
   }
 });
-
-// let ani1 = gsap.timeline();
-// ani1.from(".service .tit_box", { autoAlpha: 0, x: -100 });
-// ScrollTrigger.create({
-//   animation: ani1,
-//   trigger: ".service",
-//   start: "top",
-// });
-
-// let ani1 = gsap.timeline();
-// ani1
-//   .to(".scroll_img ", {
-//     scale: 0,
-//     rotation: 720,
-//     borderRadius: 200,
-//   })
-//   .to("#section1 .item_img", {
-//     scale: 1,
-//     rotation: 720,
-//     borderRadius: 20,
-//   });
-// ScrollTrigger.create({
-//   animation: ani1,
-//   trigger: "#section1",
-//   start: "top top ",
-//   end: "+=2000",
-//   //#section1 시작위치에서 2000픽셀 떨어진 곳에서 끝나도록 설정
-//   // markers: true,
-//   scrub: true,
-//   pin: true,
-// });
-// let ani2 = gsap.timeline({
-//   defaults: { autoAlpha: 0, borderRadius: 200 },
-// });
-// ani2
-//   .from("#section2 .i1", { y: -100 })
-//   .from("#section2 .i2", { y: 100 })
-//   .from("#section2 .i3", { y: -100 });
-// ScrollTrigger.create({
-//   animation: ani2,
-//   trigger: "#section2",
-//   start: "top top ",
-//   end: "+=2000",
-//   //#section1 시작위치에서 2000픽셀 떨어진 곳에서 끝나도록 설정
-//   // markers: true,
-//   scrub: true,
-//   pin: true,
-// });
-
-// let ani3 = gsap.timeline();
-// ani3.from("#section3 .item_img", {
-//   autoAlpha: 0,
-//   y: -100,
-//   ease: "back.out(4)",
-//   stagger: { amount: 3, from: "random" },
-// });
-// ScrollTrigger.create({
-//   animation: ani3,
-//   trigger: "#section3",
-//   start: "top top ",
-//   end: "+=3000",
-//   // markers: true,
-//   scrub: true,
-//   pin: true,
-//   anticipatePin: 1,
-// });
-
-// let ani4 = gsap.timeline();
-// ani4.to("#section4 .item_img", {
-//   autoAlpha: 0,
-//   scale: 3,
-//   width: "100vw",
-//   height: "100vh",
-// });
-// ScrollTrigger.create({
-//   animation: ani4,
-//   trigger: "#section4",
-//   start: "top top ",
-//   end: "+=3000",
-//   // markers: true,
-//   scrub: true,
-//   pin: true,
-//   anticipatePin: 1,
-// });
